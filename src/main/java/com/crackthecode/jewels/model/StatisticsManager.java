2fbafa9c06a2e08b0bdfbd4a907f52d0b08abed8
miggiegarciadev <miggiegarciadev@gmail.com>
2023-01-10 07:25:47 -0800

    worked on ticket "Statistic Manager"

--- src/main/java/com/crackthecode/jewels/model/StatisticsManager.java
+++ src/main/java/com/crackthecode/jewels/model/StatisticsManager.java
@@ -1,5 +1,32 @@
 package com.crackthecode.jewels.model;
 
 public class StatisticsManager {
+  private int loss;
+  private int win;
+  private double sucessRate;
+  private double guessAvg;
+
+  public StatisticsManager(){
+    loss = 0;
+    win = 0;
+    sucessRate = 0;
+    guessAvg = 0;
+  }
+
+  public void playGame(boolean won, int numOfGuess) {
+    if (won) {
+      win = win + 1;
+    } else {
+      loss = loss - 1;
+    }
+    sucessRate = (double) win / (win +loss);
+    guessAvg = guessAvg * (win + loss) + numOfGuess) / (win + loss + 1);
+
+@Override
+    public String toString(); {
+      return "Wins:" + win + "Losses: " + loss + "Success Rate: " + sucessRate + "Guess Avg: "
+          + guessAvg;
+    }
+    }
 
 }
