8b51d7d4842a9c107c0c33b2a0bd2725b462fe4c
sambreece <Samantha.Breece@tlgcohort.com>
2023-01-09 17:01:25 -0800

    Initial draft of Guess class

--- src/main/java/com/crackthecode/jewels/model/Guess.java
+++ src/main/java/com/crackthecode/jewels/model/Guess.java
@@ -1,5 +1,44 @@
 package com.crackthecode.jewels.model;
 
+import java.io.PrintStream;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Guess {
 
+  public static final int GUESS_LENGTH = 5;
+  public static final int NUMBER_OF_TRIES = 15;
+  public static final String INVALID_INPUT_LENGTH_MESSAGE_FORMAT = "Invalid guess: %s. Guesses must be 5 characters long. %n";
+  public static final String INVALID_CHARACTERS_MESSAGE_FORMAT = "Invalid guess:%s. Guesses for this difficulty setting may only contain the following characters: %n%s%n";
+  public static final String DUPLICATE_GUESS_MESSAGE_FORMAT = "Invalid guess:%s. This guess has already been used in this game. %n";
+
+  private String currentGuess;
+  private Set<String> guessSet = new HashSet<>();
+  private String pool;
+
+  public Guess(Cipher cipher) {
+    this.pool = cipher.getPool();
+  }
+
+
+  public void setCurrentGuess(String currentGuess) {
+    if (currentGuess.length() == 5){
+      if (currentGuess.matches(pool)) {
+        if(guessSet.add(currentGuess)) {
+          this.currentGuess = currentGuess;
+        } else {
+          throw new IllegalArgumentException(String.format(DUPLICATE_GUESS_MESSAGE_FORMAT,currentGuess));
+        }
+      } else {
+        throw new IllegalArgumentException(String.format(INVALID_CHARACTERS_MESSAGE_FORMAT,currentGuess, pool));
+      }
+    } else {
+      throw new IllegalArgumentException(String.format(INVALID_INPUT_LENGTH_MESSAGE_FORMAT,currentGuess));
+    }
+  }
+
+
+  public Set<String> getGuessSet() {
+    return guessSet;
+  }
 }
---
+++ src/test/java/com/crackthecode/jewels/model/GuessTest.java
@@ -0,0 +1,9 @@
+package com.crackthecode.jewels.model;
+
+import org.junit.jupiter.api.Test;
+
+public class GuessTest {
+
+//to be completed when we have enough classes to test the Guess class
+
+}
