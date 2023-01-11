3f7b213f684d18ffcafc5a7a455f38bf4704ab37
miggiegarciadev <miggiegarciadev@gmail.com>
2023-01-11 14:01:27 -0800

    created test for cipher test using lambda, for (difficulty level) InvalidLevel and PoolLevel

--- src/test/java/com/crackthecode/jewels/model/CipherTest.java
+++ src/test/java/com/crackthecode/jewels/model/CipherTest.java
@@ -7,36 +7,35 @@ import org.junit.jupiter.api.Test;
 
 class CipherTest {
 
-//  @Test
-//  void generateCipher() {
-//
-//  }
 
-//  @Test
-//  void getCurrentCipher() {
-//  }
 
+  //test case for if invalid input with difficultyLevel
   @Test
-  public void testAssignPool_LevelOne_poolIsLevelTwoPool() throws InvalidLevelException(){
+  public void testInvalidLevel() {
+    assertThrows(InvalidLevelException.class, () -> new Cipher(-12));
+
+  }
+
+
+ // testing for pool level
+  @Test
+  public void testAssignPool_LevelOne_poolIsLevelOnePool() {
     Cipher cipher = new Cipher(1);
-    cipher.assignPool(); //made assignPool package private from intellij suggestion to access
 
-    assertTrue(cipher.getPool().equals(Cipher.LEVEL_ONE_POOL));
+    assertEquals(Cipher.LEVEL_ONE_POOL, cipher.getPool());
   }
 
   @Test
-  public void testAssignPool_levelTwo_poolIsLevelTwoPool() throws InvalidLevelException {
+  public void testAssignPool_levelTwo_poolIsLevelTwoPool()  {
   Cipher cipher = new Cipher(2);
-  cipher.assignPool();//made assignPool package private from intellij suggestion to access
 
-  assertTrue(cipher.getPool().equals(Cipher.LEVEL_TWO_POOL.toLowerCase()));
+    assertEquals(Cipher.LEVEL_TWO_POOL, cipher.getPool());
   }
 
   @Test
-  public void testAssignPool_levelThree_poolisLevelThreePool() throws InvalidLevelException {
+  public void testAssignPool_levelThree_poolIsLevelThreePool()  {
     Cipher cipher = new Cipher(3);
-    cipher.assignPool();
 
-    assertTrue(cipher.getPool().equals(Cipher.LEVEL_THREE_POOL().toLowerCase));
+    assertEquals(Cipher.LEVEL_THREE_POOL, cipher.getPool());
   }
 }
